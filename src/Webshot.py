0a8e617aa165c74af8957801ea485cc8e04fb5fe
Luo Zhihao <1494181792@qq.com>
2021-12-23 23:35:04 +0800

    refactor: change api

--- src/Webshot.py
+++ src/Webshot.py
@@ -1,31 +1,12 @@
-from typing import Dict
 from playwright.sync_api import sync_playwright, ViewportSize
 import time
 from moviepy.editor import *
+from typing import Tuple
+from os import path
+import click
 
 
-def shootGif(html: str,  output: str, duration: float, fps: float = 40, size: ViewportSize = None, tmp_dir: str = '__WEBSHOT_GIF_TMP__'):
-    """
-    record html content as gif
-
-    Parameters    
-    ----------
-    html: html content string
-    output: output path
-    duration: duration to record
-    fps: gif fps
-    size: ViewportSize:{ width: int,height: int }
-    tmp_dir: temporary output dir
-    """
-
-    tmp_mv = os.path.normpath(os.path.join(
-        output, '..', '__WEBSHOT_GIF_TMP__.webm'))
-    shootWebm(html, tmp_mv, duration, size, tmp_dir)
-    shootGif(tmp_mv, output, fps)
-    os.remove(tmp_mv)
-
-
-def shootGif(video: str, output: str, fps: float = 40):
+def videoToGif(video: str, output: str, fps: float = 40):
     """
     convert video to gif
 
@@ -37,30 +18,49 @@ def shootGif(video: str, output: str, fps: float = 40):
     """
     clip = VideoFileClip(video)
     clip.write_gif(output, fps=fps)
+    clip.close()
 
 
-def shootWebm(html: str,  output: str, duration: float, size: ViewportSize = None, tmp_dir: str = '__WEBSHOT_WEBM_TMP__'):
+def shootWebmFromStr(html: str,  output: str, duration: float, size: ViewportSize = None, tmp_dir: str = '__WEBSHOT_WEBM_TMP__'):
     """
     record html content as webm
 
     Parameters    
     ----------
-    html: html content string
+    html: html content
     output: output path
     duration: duration to record
     size: ViewportSize:{ width: int,height: int }
     tmp_dir: temporary output dir
     """
     with sync_playwright() as p:
-        browser = p.webkit.launch(slow_mo=0)
+        browser = p.webkit.launch()
         page = browser.new_page(
-            record_video_dir=tmp_dir, record_video_size=size)
+            record_video_dir=tmp_dir, record_video_size=size, viewport=size)
         page.set_content(html)
         time.sleep(duration)
         page.close()
         page.video.save_as(output)
-        browser.close()
         _rmdir(tmp_dir)
+        browser.close()
+
+
+def shootWebmFromFile(file: str,  output: str, duration: float, size: ViewportSize = None, tmp_dir: str = '__WEBSHOT_WEBM_TMP__'):
+    """
+    record html content from file and save as webm
+
+    Parameters    
+    ----------
+    file: input file path
+    output: output path
+    duration: duration to record
+    size: ViewportSize:{ width: int,height: int }
+    tmp_dir: temporary output dir
+    """
+    html_file = open(file, encoding='utf-8')
+    html = html_file.read()
+    html_file.close()
+    shootWebmFromStr(html, output, duration, size, tmp_dir)
 
 
 def _rmdir(path):
@@ -70,3 +70,25 @@ def _rmdir(path):
         for name in dirs:
             os.rmdir(os.path.join(root, name))
     os.rmdir(path)
+
+
+@click.command('webshoot')
+@click.argument('file', type=str, required=True)
+@click.option('--output', '-o', type=str)
+@click.option('--duration', '-d', required=True, type=float)
+@click.option('--size', '-s', nargs=2, type=int)
+@click.option('--tmp', type=str)
+def webshot(file: str,  output: str, duration: float, size: Tuple[int, int], tmp: str):
+    if output == None:
+        name, ext = path.splitext(path.basename(file))
+        output = name+'.webm'
+    if tmp == None:
+        tmp = '__WEBSHOT_WEBM_TMP__'
+    click.echo('start...')
+    shootWebmFromFile(file, output, duration, None if size == None else {
+                      'width': size[0], 'height': size[1]}, tmp)
+    click.echo('write file: '+output)
+
+
+if __name__ == '__main__':
+    webshot()
